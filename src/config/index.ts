fa176d66911d3398bb7f7265e3f251ff9b2b294e
Kingsley Victor <javaprodigy56@gmail.com>
2020-09-13 02:04:58 +0100

    New commit

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import logger from "morgan";
 import router from "../router";
+import { cors } from "../middlewares";
 
 export default (app: express.Application): express.Application => {
  app.use(express.json());
  app.use(express.urlencoded({
   extended: false
  }));
+ app.use(cors("*"));
  app.use(logger("dev"));
  app.use("/api/v1", router);
  return app;
