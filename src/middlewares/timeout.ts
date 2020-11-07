6d20f782645f993d205bd49342f018c231ee929c
Kingsley Victor <javaprodigy56@gmail.com>
2020-11-07 11:27:07 +0100

    update files

--- src/middlewares/timeout.ts
+++ src/middlewares/timeout.ts
@@ -6,8 +6,18 @@ export const timeout = (timeout: number = 40000) => {
   res: express.Response, 
   next: express.NextFunction
  ) => {
-  req.setTimeout(timeout, () => console.log("Request has timed out"));
-  res.setTimeout(timeout, () => console.log("Request has timed out"));
+  req.setTimeout(timeout, () => {
+   res.status(503).json({
+    statusCode: 503,
+    response: "Request has timed out."
+   });
+  });
+  res.setTimeout(timeout, () => {
+   res.status(503).json({
+    statusCode: 503,
+    response: "Response has timed out."
+   });
+  });
   next();
  };
 };
