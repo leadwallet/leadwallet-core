85cf2b27092c3a1f6c810f1e3316d1f590f439eb
Kingsley Victor <javaprodigy56@gmail.com>
2020-11-21 19:48:44 +0100

    new commit

--- src/core/handlers/dot.ts
+++ src/core/handlers/dot.ts
@@ -1,4 +1,4 @@
-import { ApiPromise, Keyring } from "@polkadot/api";
+import { ApiPromise, Keyring, WsProvider } from "@polkadot/api";
 import cryptoRandom from "crypto-random-string";
 
 const environment = process.env.NODE_ENV;
@@ -11,7 +11,8 @@ const formats = {
 };
 
 const ss58Format = formats[environment];
-const promise = () => ApiPromise.create();
+const provider = new WsProvider("");
+const promise = () => ApiPromise.create({ provider });
 
 export class DOT {
  static async generateAddress(
@@ -38,4 +39,51 @@ export class DOT {
    return Promise.reject(new Error(error.message));
   }
  }
+
+ static async getAddressDetails(
+  address: string
+ ): Promise<{ statusCode: number; payload: any }> {
+  try {
+   const api = await promise();
+   const account = await api.query.system.account(address);
+   const { data: balance } = account;
+   return Promise.resolve({
+    statusCode: 200,
+    payload: {
+     balance: balance.free.toNumber()
+    }
+   });
+  } catch (error) {
+   return Promise.reject(new Error(error.message));
+  }
+ }
+
+ static async sendToken(
+  from: string,
+  to: string,
+  value: number
+ ): Promise<{ statusCode: number; payload: any }> {
+  try {
+   const api = await promise();
+   const keyring = new Keyring({ type: "sr25519", ss58Format });
+   const pair = keyring.addFromAddress(from);
+   const txHash = await api.tx.balances.transfer(to, value).signAndSend(pair);
+   return Promise.resolve({
+    statusCode: 200,
+    payload: {
+     hash: txHash.toString()
+    }
+   });
+  } catch (error) {
+   return Promise.reject(new Error(error.message));
+  }
+ }
+
+ // static async getTransactions(address: string) {
+ //  try {
+ //   const api = await promise();
+ //   api.tx.
+ //   const account = await api.query.system.account(address);
+ //  } catch (error) {}
+ // }
 }
