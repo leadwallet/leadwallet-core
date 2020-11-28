75b2f18fde962f01944b7f39af8e41451eb4fc42
Kingsley Victor <javaprodigy56@gmail.com>
2020-11-28 21:15:14 +0100

    add celo transactions retrieval

--- src/controllers/wallet.ts
+++ src/controllers/wallet.ts
@@ -1090,6 +1090,12 @@ export class WalletController {
      statusCode: 200,
      response: response.payload
     });
+   } else if (ticker.toLowerCase() === "celo") {
+    const response = await CELO.getTransactions(address);
+    res.status(200).json({
+     statusCode: 200,
+     response: response.payload
+    });
    }
   } catch (error) {
    res.status(error.code || 500).send(error.message);
--- src/core/handlers/celo.ts
+++ src/core/handlers/celo.ts
@@ -1,11 +1,23 @@
 import Web3 from "web3";
 import * as ContractKit from "@celo/contractkit";
+import rp from "request-promise";
 import { Environment } from "../../env";
 
 const environment = process.env.NODE_ENV;
 
 const web3 = new Web3();
 const kit = ContractKit.newKit(Environment.CELO[environment]);
+
+const testnet_scan = "https://alfajores-blockscout.celo-testnet.org/api";
+const mainnet_scan = "https://explorer.celo.org/api";
+const explorers = {
+ development: testnet_scan,
+ production: mainnet_scan,
+ test: testnet_scan,
+ staging: testnet_scan
+};
+
+const explorer = explorers[environment];
 
 export class CELO {
  static async createAddress(
@@ -65,6 +77,59 @@ export class CELO {
     payload: {
      hash: receipt.transactionHash
     }
+   });
+  } catch (error) {
+   return Promise.reject(new Error(error.message));
+  }
+ }
+
+ static async importWallet(
+  pk: string
+ ): Promise<{ statusCode: number; payload: any }> {
+  try {
+   const account = web3.eth.accounts.privateKeyToAccount(pk);
+   return Promise.resolve({
+    statusCode: 200,
+    payload: {
+     address: account.address,
+     privateKey: account.privateKey
+    }
+   });
+  } catch (error) {
+   return Promise.reject(new Error(error.message));
+  }
+ }
+
+ static async getTransactions(
+  address: string
+ ): Promise<{ statusCode: number; payload: any }> {
+  try {
+   const res = await rp.get(
+    explorer + "?module=account&action=tokentx&address=" + address,
+    {
+     simple: true,
+     json: true,
+     resolveWithFullResponse: true
+    }
+   );
+   const mappedArray = res.body.result
+    .filter((t: any) => t.tokenSymbol !== "cUSD")
+    .map((x: any) => ({
+     from: x.from,
+     to: x.to,
+     amount:
+      x.from.toLowerCase() === address.toLowerCase()
+       ? "-" + parseFloat(x.value) / 10 ** 18
+       : "+" + parseFloat(x.value) / 10 ** 18,
+     hash: x.hash,
+     status: parseInt(x.confirmations) > 0 ? "Confirmed" : "Pending",
+     nonce: parseInt(x.nonce),
+     date: new Date(parseInt(x.timeStamp))
+    }));
+
+   return Promise.resolve({
+    statusCode: 200,
+    payload: mappedArray
    });
   } catch (error) {
    return Promise.reject(new Error(error.message));
