1e13afa86f6e27506945be5b2a6c239aa064822c
Kingsley Victor <javaprodigy56@gmail.com>
2020-12-04 10:38:02 +0100

    new commit

--- src/core/handlers/bnb.ts
+++ src/core/handlers/bnb.ts
@@ -3,8 +3,8 @@ import rp from "request-promise";
 
 const environment = process.env.NODE_ENV;
 
-const bsc_mainnet = "https://bsc.dataseed1.binance.org";
-const bsc_testnet = "https://data-seed-prebsc-1-s1.binance.org";
+const bsc_mainnet = "https://bsc.dataseed.binance.org";
+const bsc_testnet = "https://data-seed-prebsc-1-s1.binance.org:8545";
 
 const bsc = {
  development: bsc_testnet,
@@ -50,9 +50,9 @@ export class BNB {
   address: string
  ): Promise<{ statusCode: number; payload: any }> {
   try {
-   console.log("=========" + address)
+   // console.log("=========" + address)
    const balance = await web3.eth.getBalance(address);
-   console.log(balance);
+   // console.log(balance);
    // console.log(JSON.stringify(balance));
    return Promise.resolve({
     statusCode: 200,
