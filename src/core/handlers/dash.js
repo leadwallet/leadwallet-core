fc6a8738dfb03bb01bdc1e74d92355da1b48520d
Kingsley Victor <javaprodigy56@gmail.com>
2021-01-17 11:16:29 +0100

    revert--

---
+++ src/core/handlers/dash.js
@@ -0,0 +1,8 @@
+// import * as dashcore from "@dashevo/dashcore-lib";
+
+// const pk = dashcore.PrivateKey.fromRandom("")
+
+// pk.toAddress().toString();
+// pk.toWIF();
+
+// const tx = new dashcore.Transaction.sighash({});
